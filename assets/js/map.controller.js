d3d78b637791efbcdcb73ef4d482e2023649d5f8
Peter McMillan <peterm@ucsc.edu>
2015-03-23 09:14:39 -0700

    added controller logic for getting geojson

--- assets/js/map.controller.js
+++ assets/js/map.controller.js
@@ -22,17 +22,42 @@ mapApp.config(function ($routeProvider) {
 
 
 // CONTROLLERS
-mapApp.controller('homeController', ['$scope', function ($scope) {
+mapApp.controller('homeController', ['$scope', '$http', 'leafletData', function ($scope, $http, leafletData) {
+    
+    
     // show UCSC centered at zoom 13
     angular.extend($scope, {
                 center: {
                     lat: 37.00010,
                     lng: -122.06001,
-                    zoom: 13
+                    zoom: 16
                 }
-	});
+    }); // end extend
+    
 
-}]);
+    
+    // get geoJSON file and process
+    $scope.ucscBuildings = {};
+    
+    $http.get('data/ucsc.buildings.geojson').
+    success(function(data, status, headers, config) {
+      $scope.ucscBuildings = data;
+        //console.log('got a data object' + data);
+        
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+        console.log('we gotta problem');
+    });
+    
+    // add geoJSON to map
+    leafletData.getMap().then(function(map) {
+        L.geoJson($scope.ucscBuilings).coordsToLatLng($scope.ucscBuildings).addTo(map);
+    });
+        
+                                                         
+}]); // end controller
+    
 
 mapApp.controller('forecastController', ['$scope', function ($scope) {
 
